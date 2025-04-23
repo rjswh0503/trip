4ecf791cdbd1cbd29f67130640a9f2c21861797f
rjswh0503 <rjswh0503@yahoo.com>
2025-04-23 21:33:47 +0900

    design

--- trip/client/src/reviews/page/reviewList.js
+++ trip/client/src/reviews/page/reviewList.js
@@ -34,21 +34,23 @@ const PlaceByReview = () => {
         return <p>해당 장소의 리뷰가 없습니다...</p>
     }
 
-    
+
 
     return (
         <div>
             <div>
-                <h1>리뷰</h1>
                 <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mx-auto py-[150px]  max-w-screen-2xl'>
                     {reviewList && reviewList.map(review => (
-                        <div className='' key={review._id}>
-                            <Link to={`/places/${id}/review/${review._id}`}>
-                                <h2>{review.title}</h2>
-                            </Link>
-                            <p>{review.content}</p>
-                            <p>작성자:{review.author?.name}</p>
-                            
+                        <div className='bg-white border border-gray-200 rounded-lg shadow-sm' key={review._id}>
+                            <div className='p-4 text-center'>
+                                <Link to={`/places/${id}/review/${review._id}`}>
+                                    <h5 className='text-2xl font-bold p-2 text-gray-900'>{review.title}</h5>
+                                </Link>
+                                <p className='mb-4'>{review.content}</p>
+                                <div className='flex justify-between py-5'>
+                                    <p>작성자:{review.author?.name}</p>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
