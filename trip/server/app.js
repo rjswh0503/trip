7fb477fe4e2343ce23d1f712e218dac28523a7fa
rjswh0503 <rjswh0503@yahoo.com>
2025-05-16 20:22:12 +0900

    server

--- trip/server/app.js
+++ trip/server/app.js
@@ -14,15 +14,15 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: 'https://trip-client.onrender.com', 
+    origin: 'https://trip-client.onrender.com',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true 
+    credentials: true
 }));
 
 
@@ -52,12 +52,16 @@ app.use((error, req, res, next) => {
 
 
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => {
+        console.log('MongoDB 연결 성공');
         app.listen(PORT, () => {
-            console.log(`포트번호${PORT}에서 서버가 실행중....`)
-            console.log("MONGO_URI:", process.env.MONGO_URI);
+            console.log(`서버 실행 중: http://localhost:${PORT}`);
         });
-    }).catch((err) => {
-        console.log(err);
     })
+    .catch((err) => {
+        console.error('❌ MongoDB 연결 실패:', err);
+    });
